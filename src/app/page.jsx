363d2969ee52bcd91b3a42293471d17bf16e2d98
deepak9285 <83562221+deepak9285@users.noreply.github.com>
2025-02-21 10:39:27 +0530

    added shadcdn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <Button />
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
