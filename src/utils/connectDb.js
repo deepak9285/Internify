270b05d57b0031c8b6ed48be8c56d9eed998093d
kshimate <amaymishra100@gmail.com>
2025-02-22 11:37:57 +0530

    mongoDB

--- src/utils/connectDb.js
+++ src/utils/connectDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async ()=> {
   
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI || "")
+        const db = await mongoose.connect("mongodb+srv://mongodb+srv://vaibhavgupta8245:pogmLNOZUuhiuNPf@cluster0.kk7pg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" || "")
 
 
         console.log("Database connected")
